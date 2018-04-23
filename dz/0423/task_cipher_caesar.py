75b55e77d35ffc18a8d85e0a79ec57cecf90ea00
Pavel Belyakov <palb@yandex.ru>
2018-04-23 14:16:33 +0300

    Dz update caesar

--- dz/0423/task_cipher_caesar.py
+++ dz/0423/task_cipher_caesar.py
@@ -1,8 +1,8 @@
 def encode(text, rot=0):
     alpha = 'abcdefghijklmnopqrstuvwxyz'
     code_text = ''
-    if rot > len(alpha):
-        new_rot = rot - int(rot/len(alpha)) * len(alpha) - 1
+    if rot >= len(alpha):
+        new_rot = rot - int(rot/len(alpha)) * len(alpha)
     else:
         new_rot = rot
 
@@ -28,8 +28,8 @@ def encode(text, rot=0):
     print(code_text)
     if code_text == 'Ifmmp, Qzuipo3!':
         print('ok')
-    #elif code_text == 'Hello, Python3!':
-    #    print('ok')
+    elif code_text == 'Hello, Python3!':
+        print('ok')
     elif code_text == "The cleaner and nicer the program, the faster it's going to run. And if it doesn't, it'll be easy to make it fast.":
         print('ok')
     elif code_text == 'Sgdqd hr mn oqnfqzllhmf kzmftzfd, mn lzssdq gnv rsqtbstqdc, sgzs vhkk oqdudms oqnfqzlldqr eqnl lzjhmf azc oqnfqzlr.':
@@ -44,8 +44,8 @@ def encode(text, rot=0):
 def decode(text, rot=0):
     alpha = 'abcdefghijklmnopqrstuvwxyz'
     code_text = ''
-    if rot > len(alpha):
-        new_rot = rot - int(rot/len(alpha)) * len(alpha) - 1
+    if rot >= len(alpha):
+        new_rot = rot - int(rot/len(alpha)) * len(alpha)
     else:
         new_rot = rot
 
@@ -71,8 +71,8 @@ def decode(text, rot=0):
     print(code_text)
     if code_text == 'Hello, Python3!':
         print('ok')
-    #elif code_text == 'Hello, Python3!':
-    #    print('ok')
+    elif code_text == 'Hello, Python3!':
+        print('ok')
     elif code_text == "Gur pyrnare naq avpre gur cebtenz, gur snfgre vg'f tbvat gb eha. Naq vs vg qbrfa'g, vg'yy or rnfl gb znxr vg snfg.":
         print('ok')
     elif code_text == 'There is no programming language, no matter how structured, that will prevent programmers from making bad programs.':
@@ -83,8 +83,8 @@ def decode(text, rot=0):
         print('no')
 
 
-#encode('y',31)
-encode('Hello, Python3!',0)
+encode('z',27)
+encode('Hello, Python3!',1)
 #encode('Hello, Python3!',30)
 encode("Gur pyrnare naq avpre gur cebtenz, gur snfgre vg'f tbvat gb eha. Naq vs vg qbrfa'g, vg'yy or rnfl gb znxr vg snfg.", 13)
 encode('There is no programming language, no matter how structured, that will prevent programmers from making bad programs.', 25)
@@ -92,6 +92,7 @@ encode('There is no programming language, no matter how structured, that will pr
 
 print('===================')
 
+decode('z',27)
 decode('Ifmmp, Qzuipo3!',1)
 decode("The cleaner and nicer the program, the faster it's going to run. And if it doesn't, it'll be easy to make it fast.",13)
 decode('Sgdqd hr mn oqnfqzllhmf kzmftzfd, mn lzssdq gnv rsqtbstqdc, sgzs vhkk oqdudms oqnfqzlldqr eqnl lzjhmf azc oqnfqzlr.',25)
